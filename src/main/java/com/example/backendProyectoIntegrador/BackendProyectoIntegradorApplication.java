a0f4bbaee3b5228d78dded4c6e73a674608f7cb2
ricardocuellar <rcuellarsn@gmail.com>
2025-02-15 21:16:10 -0600

    Init Project

---
+++ src/main/java/com/example/backendProyectoIntegrador/BackendProyectoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendProyectoIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProyectoIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProyectoIntegradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendProyectoIntegrador/BackendProyectoIntegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendProyectoIntegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProyectoIntegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
