f64a612805fbd9a63680c2dbea0a5c73cf05cc66
Reddmar Quevedo <reddmar.quevedo@gmail.com>
2025-02-20 12:43:42 -0500

    chore: change names for folders
    - Changed name to petcare from example.
    - Changed application name to proyectoIntegrador
    - Changed routes to match current project's structure

--- src/main/java/com/petcare/backend/proyectoIntegrador/ProyectoIntegradorApplication.java
+++ src/main/java/com/petcare/backend/proyectoIntegrador/ProyectoIntegradorApplication.java
@@ -1,13 +1,13 @@
-package com.example.backendProyectoIntegrador;
+package com.petcare.backend.proyectoIntegrador;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendProyectoIntegradorApplication {
+public class ProyectoIntegradorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendProyectoIntegradorApplication.class, args);
+		SpringApplication.run(ProyectoIntegradorApplication.class, args);
 	}
 
 }
--- src/test/java/com/petcare/backend/proyectoIntegrador/ProyectoIntegradorApplicationTests.java
+++ src/test/java/com/petcare/backend/proyectoIntegrador/ProyectoIntegradorApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.backendProyectoIntegrador;
+package com.petcare.backend.proyectoIntegrador;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendProyectoIntegradorApplicationTests {
+class ProyectoIntegradorApplicationTests {
 
 	@Test
 	void contextLoads() {
